718223194bd8e76b9224c4ab6bdbe51f6ce51506
Arman Mazhgulov <mazhgulov@gmail.com>
2024-02-18 00:17:45 +0600

    Домашнее задание Lesson1:Lection1

---
+++ Les1_lec1/Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вычислить значение формулы (a*b)/(c+d). Результат вывести
+
+int a = 1;
+int b = 2;
+int c = 5;
+int d = 1;
+
+double formula = (double)(a*b)/(c+d);
+
+// перед формулой необходимо задать тип данных!
+
+Console.WriteLine(formula);
