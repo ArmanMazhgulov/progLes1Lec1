c810835866ef288f22895751983bfa49b0fb8ea2
Arman Mazhgulov <mazhgulov@gmail.com>
2024-02-17 22:52:28 +0600

    добавили переменные

--- Les1_lec1/Task1/Program.cs
+++ Les1_lec1/Task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello world");
+﻿// Условие задачи;
+// Объявить переменную - дробное число, инициализировать ее некоторым; 
+// значением и вывести его на экран;
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+
+
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
