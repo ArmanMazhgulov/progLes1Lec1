e170dfcd060464f78423662eac6a2b069813d57d
Arman Mazhgulov <mazhgulov@gmail.com>
2024-02-20 02:00:10 +0600

    Из двухзначного числа выбрать наибольшее

---
+++ Les2_Sem/homework2_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+Console.WriteLine("Введите целое число от 10 до 99");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = num % 10;
+int secondDigit = (num - firstDigit) / 10;
+
+bool result = firstDigit > secondDigit;
+
+if (result)
+{
+    Console.WriteLine($"{firstDigit}");
+}
+else
+{
+    Console.WriteLine($"{secondDigit}");
+}
