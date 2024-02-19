497cb79c292e3a159edfbe1e30b642268cc711e0
Arman Mazhgulov <mazhgulov@gmail.com>
2024-02-19 13:19:00 +0600

    Задача- из 3значного второе в степень третьего

---
+++ Les2_Sem/TAsk2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и возводит вторую цифру этого числа в степень, равную третьей цифре.
+// Примеры
+// 487 => 8^7 = 2097152
+// 254 => 5^4 = 625
+// 617 => 1
+
+Console.WriteLine("Введите трехзначное целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999 || num >= -999 && num <= -100) // проерка на трехзначность
+{
+    int secondDigit = num / 10 % 10; // 256/100=25.6 %10 =>5
+    int thirdDigit = num % 10; // 256 % 10 = 250 +6 => 6
+
+    if (thirdDigit < 0)
+    {
+        thirdDigit = thirdDigit * -1;
+    }
+
+    int result = 1;
+    int count = 1;
+    while (count <= thirdDigit)
+    {
+        result *= secondDigit;
+        count++;
+    }
+    Console.WriteLine($"{num} => {result}");
+}
+else
+{
+    Console.WriteLine("Некорректное число");
+}
