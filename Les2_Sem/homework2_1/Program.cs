ee075b602d68b2952fd46b25b08a2f0e4b809359
Arman Mazhgulov <mazhgulov@gmail.com>
2024-02-20 01:39:31 +0600

    кратность 7 и 23

---
+++ Les2_Sem/homework2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+bool result = num%7==0 && num%23==0;
+
+if (result)
+{
+    Console.WriteLine($"{num} является кратным одновременно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"{num} НЕ является кратным одновременно 7 и 23");
+}
+
