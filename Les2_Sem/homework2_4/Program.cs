643d32eaa8a15fb09e12862df4285a4c22ba36ac
Arman Mazhgulov <mazhgulov@gmail.com>
2024-02-21 17:00:21 +0600

    Натуральное число, показывает его цифры через запятую

---
+++ Les2_Sem/homework2_4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int num = number;
+int num2 = number;
+
+while (num >= 10)
+{
+    num = num / 10;
+    count = count * 10;
+}
+
+while (count >= 10)
+{
+    num2 = number / count;
+    Console.Write($"{num2}, ");
+    number = number - num2 * count;
+    count = count / 10;
+}
+
+Console.Write($"{number}");
